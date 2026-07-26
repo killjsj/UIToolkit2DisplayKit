118e7d7d9fe570f2b80c72b11fb9ac8847c03506
killjsj <killjsj@outlook.com>
2026-07-26 13:30:42 +0800

    23

--- Assets/UXML2CODE_MAIN.cs
+++ Assets/UXML2CODE_MAIN.cs
@@ -582,13 +582,13 @@ public class UxmlToCodeWindow : EditorWindow
                 case NodeType.VisualElement:
                     sb.AppendLine(prefix + $"// start define of {varName}");
                     sb.AppendLine(prefix + $"DisplayElement {varName} = {parent.GetName()}.AddElement();");
-                    sb.AppendLine(prefix + $"{varName}.BaseElement.name = {varName};");
+                    sb.AppendLine(prefix + $"{varName}.BaseElement.name = \"{varName}\";");
                     break;
                 case NodeType.Label:
                     sb.AppendLine(prefix + $"// start define of {varName}");
                     if (!string.IsNullOrEmpty(text)) text = text.Replace("\n", "\\n");
                     sb.AppendLine(prefix + $"DisplayText {varName} = {parent.GetName()}.AddText(\"{text}\");");
-                    sb.AppendLine(prefix + $"{varName}.BaseElement.name = {varName};");
+                    sb.AppendLine(prefix + $"{varName}.BaseElement.name = \"{varName}\";");
                     break;
                 case NodeType.UXML:
                     sb.AppendLine(prefix + $"// start define of {varName}");
